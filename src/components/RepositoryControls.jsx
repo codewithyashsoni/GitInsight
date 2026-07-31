4bc91457341f5ac6d5f521486126d7e57c1dddc1
codewithyashsoni <yashsoni.contact@gmail.com>
2026-07-31 12:45:56 +0530

    Added repository controls

--- src/components/RepositoryControls.jsx
+++ src/components/RepositoryControls.jsx
@@ -1,4 +1,31 @@
-function RepositoryControls(){
-    
+function RepositoryControls({sort, setSort, filter, setFilter}){
+    return(
+        <div className="repository-controls-container">
+            <h2>Repositories: </h2>
+
+            <div class="sort-filter-container">
+
+                <select
+                    value={sort}
+                    onChange={(e) => setSort(e.target.value)}
+                    className="sort-selection"
+                >
+                    
+                    <option value="Recently Updated">Recently Updated</option>
+                    <option value="Most Stars">Most stars</option>
+                </select>
+
+                <input
+                    type="text"
+                    value={filter}
+                    onChange={(e) => setFilter(e.target.value)}
+                    placeholder="Filter repositories..."
+                    className="filter-input"
+                />
+
+            </div>
+
+        </div>
+    )   
 }
 export default RepositoryControls
--- src/components/RepositorySection.jsx
+++ src/components/RepositorySection.jsx
@@ -1,10 +1,14 @@
 import {useState} from "react"
 import RepositoryControls from "./RepositoryControls.jsx"
+import RepositoryList from "./RepositoryList.jsx"
 
 function RepositorySection({data}){
+    const [sort, setSort] = useState("Recently Updated");
+    const [filter, setFilter] = useState("");
+
     return(
         <div className="repository-section-container">
-            <RepositoryControls />
+            <RepositoryControls sort={sort} setSort={setSort} filter={filter} setFilter={setFilter} />
             <RepositoryList />
         </div>
     )
